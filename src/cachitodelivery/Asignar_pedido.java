d6be110cd0df5710052a0632a66d84ff9c98648b
Cusi <santiagoivan145@hotmail.com>
2016-11-17 13:20:11 -0300

    "16/11/2016"

--- src/cachitodelivery/Asignar_pedido.java
+++ src/cachitodelivery/Asignar_pedido.java
@@ -5,10 +5,19 @@
  */
 package cachitodelivery;
 
+import Excepciones.DataAccessException;
 import Ventana_clases.Fondo_asignar;
 import Ventana_clases.Fondo_modificar_usuario;
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import modelos.Cadena;
+import modelos.PedidoDAO;
+import modelos.Usuario;
+import modelos.UsuarioDAO;
 
 /**
  *
@@ -16,9 +25,9 @@ import javax.swing.ImageIcon;
  */
 public class Asignar_pedido extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Asignar_pedido
-     */
+    Object[][] pedidos;
+    Object[][] mochila = new Object[0][0];
+    
     public Asignar_pedido() {
         initComponents();
         Fondo_asignar fondo = new Fondo_asignar(1161,600);
@@ -33,7 +42,146 @@ public class Asignar_pedido extends javax.swing.JFrame {
         setIconImage (new ImageIcon(getClass().getResource("/Ventanas/Icono.png")).getImage());    
         setVisible(b);
     }
+    public void pedidos() throws DataAccessException{
 
+        PedidoDAO ped = new PedidoDAO();
+
+        pedidos = ped.pedidosListos();
+
+    }
+    
+    public void iniciarListado(){
+
+        jTable1.setTableHeader(null);
+        jTable2.setTableHeader(null);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+                    pedidos,new String[] {"","","",""}));
+
+        jTable1.getColumnModel().getColumn(0).setPreferredWidth(30);
+        jTable1.getColumnModel().getColumn(1).setPreferredWidth(250);
+        jTable1.getColumnModel().getColumn(2).setPreferredWidth(300);
+        jTable1.getColumnModel().getColumn(3).setPreferredWidth(90);
+
+    }
+    public void iniciarMochila(){
+
+        jTable1.setTableHeader(null);
+        jTable2.setTableHeader(null);
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+                    mochila,new String[] {"","","",""}));
+
+        jTable2.getColumnModel().getColumn(0).setPreferredWidth(30);
+        jTable2.getColumnModel().getColumn(1).setPreferredWidth(250);
+        jTable2.getColumnModel().getColumn(2).setPreferredWidth(300);
+        jTable2.getColumnModel().getColumn(3).setPreferredWidth(90);
+
+    }
+    
+    public void agregar(){
+        if (jTable1.getSelectedRow()==-1){
+            
+        }
+        if (jTable1.getValueAt(jTable1.getSelectedRow(),0)==null){
+
+        }else{
+            int i = jTable1.getSelectedRow();
+            
+            if(mochila.length==0){
+                Object[][] primero = new Object[1][4];
+                primero[0][0] = pedidos[i][0];
+                primero[0][1] = pedidos[i][1];
+                primero[0][2] = pedidos[i][2];
+                primero[0][3] = pedidos[i][3];
+                mochila=new Object[1][4];
+                mochila=primero;
+            }else{
+                int pos = mochila.length;
+                Object[][] primero = new Object[pos+1][4];
+                for (int j = 0; j < mochila.length; j++) {
+                    primero[j][0] = mochila[j][0];
+                    primero[j][1] = mochila[j][1];
+                    primero[j][2] = mochila[j][2];
+                    primero[j][3] = mochila[j][3];
+                }
+                primero[pos][0] = pedidos[i][0];
+                primero[pos][1] = pedidos[i][1];
+                primero[pos][2] = pedidos[i][2];
+                primero[pos][3] = pedidos[i][3];
+                mochila = new Object[pos+1][4];
+                mochila=primero;
+            }
+            
+            Object[][] menor = new Object[pedidos.length-1][4];
+            int k=0;
+            for (int j = 0; j < pedidos.length; j++) {
+                if(j!=i){
+                    menor[k][0] = pedidos[j][0];
+                    menor[k][1] = pedidos[j][1];
+                    menor[k][2] = pedidos[j][2];
+                    menor[k][3] = pedidos[j][3];
+                    k++;
+                }
+            }
+            pedidos = new Object[menor.length][4];
+            pedidos = menor;
+            iniciarListado();
+            iniciarMochila();
+        }
+    }
+    public void quitar(){
+        if (jTable2.getSelectedRow()==-1){
+            
+        }
+        if (jTable2.getValueAt(jTable2.getSelectedRow(),0)==null){
+
+        }else{
+            int i = jTable2.getSelectedRow();
+            
+            if(pedidos.length==0){
+                Object[][] primero = new Object[1][4];
+                primero[0][0] = mochila[i][0];
+                primero[0][1] = mochila[i][1];
+                primero[0][2] = mochila[i][2];
+                primero[0][3] = mochila[i][3];
+                pedidos=new Object[1][4];
+                pedidos=primero;
+            }else{
+                int pos = pedidos.length;
+                Object[][] primero = new Object[pos+1][4];
+                for (int j = 0; j < pedidos.length; j++) {
+                    primero[j][0] = pedidos[j][0];
+                    primero[j][1] = pedidos[j][1];
+                    primero[j][2] = pedidos[j][2];
+                    primero[j][3] = pedidos[j][3];
+                }
+                primero[pos][0] = mochila[i][0];
+                primero[pos][1] = mochila[i][1];
+                primero[pos][2] = mochila[i][2];
+                primero[pos][3] = mochila[i][3];
+                pedidos = new Object[pos+1][4];
+                pedidos=primero;
+            }
+            
+            Object[][] menor = new Object[mochila.length-1][4];
+            int k=0;
+            for (int j = 0; j < mochila.length; j++) {
+                if(j!=i){
+                    menor[k][0] = mochila[j][0];
+                    menor[k][1] = mochila[j][1];
+                    menor[k][2] = mochila[j][2];
+                    menor[k][3] = mochila[j][3];
+                    k++;
+                }
+            }
+            mochila = new Object[menor.length][4];
+            mochila = menor;
+            iniciarListado();
+            iniciarMochila();
+        }
+    }
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -47,10 +195,6 @@ public class Asignar_pedido extends javax.swing.JFrame {
         JL_Usuario_admin1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jLabel4 = new javax.swing.JLabel();
@@ -71,25 +215,28 @@ public class Asignar_pedido extends javax.swing.JFrame {
         JL_Usuario_admin1.setText("USUARIO: ");
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
+            null,
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
+        try
+        {
+            pedidos();
+            iniciarListado();
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
+            null,
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
@@ -102,6 +249,11 @@ public class Asignar_pedido extends javax.swing.JFrame {
         jButton1.setContentAreaFilled(false);
         jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton1.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Botones/Agregar_caja_hover.png"))); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Botones/Quitar2.png"))); // NOI18N
         jButton2.setBorder(null);
@@ -109,6 +261,11 @@ public class Asignar_pedido extends javax.swing.JFrame {
         jButton2.setContentAreaFilled(false);
         jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton2.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Botones/Quitar2_hover.png"))); // NOI18N
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         JL_Fecha_Admin1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         JL_Fecha_Admin1.setForeground(new java.awt.Color(255, 255, 255));
@@ -136,15 +293,7 @@ public class Asignar_pedido extends javax.swing.JFrame {
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 340, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jScrollPane1)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 807, Short.MAX_VALUE)
                             .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jScrollPane2))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -172,10 +321,8 @@ public class Asignar_pedido extends javax.swing.JFrame {
                                 .addComponent(jLabel15, javax.swing.GroupLayout.PREFERRED_SIZE, 920, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(JL_Hora_Admin1)))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 897, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGap(0, 259, Short.MAX_VALUE))
+                    .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 897, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -188,17 +335,15 @@ public class Asignar_pedido extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(jButton1)
+                        .addGap(121, 121, 121))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -216,10 +361,20 @@ public class Asignar_pedido extends javax.swing.JFrame {
                 .addGap(187, 187, 187))
         );
 
-        jTextField3.getAccessibleContext().setAccessibleName("");
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTable1MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        agregar();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        quitar();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -273,9 +428,5 @@ public class Asignar_pedido extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
