e3c177bcb936fc73516176a263ecb7c5732e2888
flaviopifiator <flaviomariano1993@hotmail.com>
2016-11-17 04:02:20 -0300

    eewg

--- src/modelos/BaseDeDatos.java
+++ src/modelos/BaseDeDatos.java
@@ -20,7 +20,7 @@ public class BaseDeDatos {
             String url = "jdbc:postgresql://localhost:5432/cachito";  
             String driver = "org.postgresql.Driver";
             String user = "postgres";
-            String pass = "postgres";
+            String pass = "1234";
             Class.forName(driver);
             instance = DriverManager.getConnection(url, user, pass);
         }
